732b2bcc593ad59fe993b8a33d770eeebe5824e9
cranoo <75974473+cranoo3@users.noreply.github.com>
2024-03-20 22:35:45 +0900

    Initial Commit

---
+++ KeyChainSample/KeyChainSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyChainSampleApp.swift
+//  KeyChainSample
+//
+//  Created by cranoo on 2024/03/20.
+//
+
+import SwiftUI
+
+@main
+struct KeyChainSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
